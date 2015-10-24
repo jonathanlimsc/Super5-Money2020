bcb4e0516dcd32d37df3e06228299260a924c5a6
davis12529 <davis12529@hotmail.com>
2015-10-24 15:46:16 -0700

    skeleton structure for rendering html on express

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,7 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  console.log("get");
+  //It will find and locate index.html from View or Scripts
+});
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -56,5 +61,11 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
 
 module.exports = app;
