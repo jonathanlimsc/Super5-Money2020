a7d3e21734df29816c3e18b4c78c72ffba053624
davis12529 <davis12529@hotmail.com>
2015-10-25 12:41:48 -0700

    fix production facebook callback url

--- config/env/production.js
+++ config/env/production.js
@@ -6,6 +6,6 @@ module.exports = {
     facebook: {
       app_id: "466921426844612",
       app_secret: "d596fc9b80a60b26d7b4182f41503cd0",
-      callback_url: "http://tobefilled.com/user/auth/facebook/callback/"
+      callback_url: "http://merchain.herokuapp.com/user/auth/facebook/callback/"
     }
 };
