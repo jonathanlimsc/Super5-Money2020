4669b653466b0768fdf6d56d3952e25b3e08eb3a
davis12529 <davis12529@hotmail.com>
2015-10-25 11:03:21 -0700

    added mongolab hosting for production

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@ var port = 80;
 
 module.exports = {
     port: port,
-    // db: 'mongodb://localhost/yelpvotingdb'
+    db: 'mongodb://admin:admin@ds037587.mongolab.com:37587/merchaindb'
     facebook: {
       app_id: "466921426844612",
       app_secret: "d596fc9b80a60b26d7b4182f41503cd0",
