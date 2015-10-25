702e6d3b4806727f8f64100960603e784eaa1ba2
davis12529 <davis12529@hotmail.com>
2015-10-25 11:20:06 -0700

    mongo connection for heroku mongolab addon

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@ var port = 80;
 
 module.exports = {
     port: port,
-    db: 'mongodb://admin:admin@ds037587.mongolab.com:37587/merchaindb'
+    db: 'mongodb://admin:admin@ds045454.mongolab.com:45454/heroku_dm9g606d'
     facebook: {
       app_id: "466921426844612",
       app_secret: "d596fc9b80a60b26d7b4182f41503cd0",
